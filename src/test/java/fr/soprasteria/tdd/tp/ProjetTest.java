8876d22e5255935b8ed31e2209d20552ba09c29a
Tony TAN <73937672+Enfant-de-lumiere@users.noreply.github.com>
2023-12-11 15:12:23 +0100

    projet

---
+++ src/test/java/fr/soprasteria/tdd/tp/ProjetTest.java
@@ -0,0 +1,16 @@
+package fr.soprasteria.tdd.tp;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class ProjetTest {
+	
+	@Test
+	void testVide() {
+		
+		
+	}
+
+}
